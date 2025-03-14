9a3698785ca44aae27b2d5dd33f142baf5fd2e36
김준영 <127184534+WaDDaDDa@users.noreply.github.com>
2025-03-14 19:11:31 +0900

    feat : Player 이동, 타일맵 구현

---
+++ Assets/Script/PlayerInputController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = mainCamera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+}
---
+++ Assets/Script/TopDownMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody;
+
+    private Vector2 movemnetDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movemnetDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovemnet(movemnetDirection);
+    }
+
+    private void ApplyMovemnet(Vector2 direction)
+    {
+        direction = direction * 5;
+        movementRigidbody.linearVelocity = direction;
+    }
+}
